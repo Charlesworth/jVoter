caf7afeabd645fb99861cc52bd7cacc8c3058c44
Charlie <cochrane26@gmail.com>
2015-02-19 01:59:04 +0000

    added getters and setter methods to influx DB

--- src/main/java/sample/hello/InfluxCapacitor.java
+++ src/main/java/sample/hello/InfluxCapacitor.java
@@ -9,19 +9,35 @@ import org.influxdb.dto.Serie;
 import akka.actor.UntypedActor;
 
 public class InfluxCapacitor extends UntypedActor{
-
+	InfluxDB influxDB = InfluxDBFactory.connect("http://localhost:8086", "root", "root");
+	
+	@Override
+	public void onReceive(Object msg) throws Exception {
+		if (msg == "dbtest"){
+			dbtest();
+		} if (msg == "setVote"){
+			
+		} if (msg == "setComment"){
+			
+		} if (msg == "setDescription"){
+			
+		} if (msg == "getStatus"){
+		
+		} else {
+		unhandled(msg);
+		}
+	}
 	
 	void dbtest(){
-		System.out.println("DB test");
+		System.out.println(TimeUnit.MILLISECONDS);
 		
-		InfluxDB influxDB = InfluxDBFactory.connect("http://localhost:8086", "root", "root");
+		
 
 		//influxDB.createDatabase("testerDB");
 
 		Serie serie1 = new Serie.Builder("farty6")
 		            .columns("testcolumn1", "testcolumn2")
 		            .values(System.currentTimeMillis(), 1)
-		            .values(System.currentTimeMillis(), 2)
 		            .build();
 		try {
 		influxDB.write("testerDB", TimeUnit.MILLISECONDS, serie1);
@@ -29,16 +45,36 @@ public class InfluxCapacitor extends UntypedActor{
 			System.out.println(e);
 		}
 	}
-
-	@Override
-	public void onReceive(Object msg) throws Exception {
-		if (msg == "dbtest"){
-			dbtest();
-		}
+	
+	void setVote(String id, boolean vote){
+		Serie serie = new Serie.Builder("vote")
+        	.columns("vote")
+        	.values(vote)
+        	.build();
 		
+		influxDB.write(id, TimeUnit.MILLISECONDS, serie);
 	}
 	
+	void setComment(String id, String comment){
+		Serie serie = new Serie.Builder("comment")
+    		.columns("comment")
+    		.values(comment)
+    		.build();
 	
+		influxDB.write(id, TimeUnit.MILLISECONDS, serie);		
+	}
 	
-	
+	void setDescription(String id, String description){
+		Serie serie = new Serie.Builder("description")
+			.columns("description")
+			.values(description)
+			.build();
+
+		influxDB.write(id, TimeUnit.MILLISECONDS, serie);		
+	}
+
+	void getStatus(String id){
+		System.out.println("Not set yet");
+	}
+		
 }
